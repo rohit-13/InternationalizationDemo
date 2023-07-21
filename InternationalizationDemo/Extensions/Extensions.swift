aeb68543cb309e26070bb137e7359c4df45af1f3
Rohit Kumar <rohit.kumar@yellow.ai>
2023-07-21 13:09:52 +0530

    starter-project added

---
+++ InternationalizationDemo/Extensions/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  InternationalizationDemo
+//
+//  Created by Rohit Kumar on 21/07/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func showErrorAlert(_ error: String) {
+        let alert = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showInfoAlert(_ message: String) {
+        let alert = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+}
---
+++ InternationalizationDemo/Views/SigninViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SigninViewController.swift
+//  InternationalizationDemo
+//
+//  Created by Rohit Kumar on 21/07/23.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func signinBtnPressed(_ sender: Any) {
+        if let email = emailTextField.text, let password = passwordTextField.text, !email.isEmpty, !password.isEmpty {
+            if KeychainHelper.shared.checkPassword(email: email, password: password) {
+                guard let welcomeVC = storyboard?.instantiateViewController(identifier: "welcomeVC") as? WelcomeViewController else { return }
+                welcomeVC.user = email
+                navigationController?.pushViewController(welcomeVC, animated: true)
+            }
+            else {
+                self.showErrorAlert("Invalid Credentials")
+            }
+        }
+    }
+}
